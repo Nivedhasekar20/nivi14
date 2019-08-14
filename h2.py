5977e8042086958cbb73d192136eba0849739fb8
Nivedhasekar20 <52271674+Nivedhasekar20@users.noreply.github.com>
2019-08-14 13:42:35 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -1,4 +1,4 @@
-t12 = int(input())
-n12 = [x for x in input().split()]
-m12= sorted(n12, reverse=True)
+t11 = int(input())
+n11 = [x for x in input().split()]
+m11= sorted(n11, reverse=True)
 print(''.join(m12))
